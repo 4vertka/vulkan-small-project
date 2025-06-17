f4c5cb2e94c4f615c36f1d4348cac4f7d79c6221
Alex <sashapisarchuk@gmail.com>
2025-06-17 08:45:40 +0000

    changed matrices for 2d

--- src/engine.cpp
+++ src/engine.cpp
@@ -821,13 +821,23 @@ void VulkanEngine::updateUniformBuffer(uint32_t currentImage) {
   ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f),
                           glm::vec3(0.0f, 0.0f, 1.0f));
 
-  ubo.view =
-      glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f),
-                  glm::vec3(0.0f, 0.0f, 1.0f));
+  // ubo.view =
+  //     glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f),
+  //                 glm::vec3(0.0f, 0.0f, 1.0f));
 
-  ubo.proj = glm::perspective(
-      glm::radians(45.0f),
-      _swapchainExtent.width / (float)_swapchainExtent.height, 0.1f, 10.0f);
+  ubo.view =
+      glm::lookAt(glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f),
+                  glm::vec3(0.0f, 1.0f, 0.0f));
+
+  float aspect = _swapchainExtent.width / (float)_swapchainExtent.height;
+  float orthoSize = 2.0f;
+
+  ubo.proj = glm::ortho(-orthoSize * aspect, orthoSize * aspect, -orthoSize,
+                        orthoSize, -1.0f, 1.0f);
+
+  // ubo.proj = glm::perspective(
+  //     glm::radians(45.0f),
+  //    _swapchainExtent.width / (float)_swapchainExtent.height, 0.1f, 10.0f);
 
   ubo.proj[1][1] *= -1;
 
